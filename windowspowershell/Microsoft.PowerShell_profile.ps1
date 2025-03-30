8bd990d6ba2a6a79f5c60109e1d753f77458c7bd
Johan <47191187+johansource@users.noreply.github.com>
2025-03-30 15:52:02 +0200

    Updated default directory of Powershell profile

--- windowspowershell/Microsoft.PowerShell_profile.ps1
+++ windowspowershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 # Initialise Starship with PowerShell
 Invoke-Expression (&starship init powershell)
 
-# Default directory
-Set-Location C:\Projects
+# Set default directory only if starting in $HOME
+if ($PWD.Path -eq $HOME) {
+  Set-Location C:\Projects
+}
